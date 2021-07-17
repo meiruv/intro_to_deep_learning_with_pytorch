6efcdb91fa2cd7f01ed985daccf98674db0fede1
meiruv <meirav58@gmail.com>
2021-07-17 16:30:52 +0300

    ex1 with answers to my questions

--- ex_1/my_solution.py
+++ ex_1/my_solution.py
@@ -19,14 +19,17 @@ def prediction(X, W, b):
 # and return W and b.
 def perceptronStep(X, y, W, b, learn_rate = 0.01):
     for i in range(len(X)):
-        y_pred = prediction(prediction(X[i],W,b))
-        if(y[i]!=y_pred):
-            if(y_pred == 0):
-                W[i] = W[i] + X[i]*learn_rate
-                b[i] = b[i] + learn_rate
-            elif(y_pred == 1):
-                W[i] = W[i] - X[i]*learn_rate
-                b[i] = b[i] - learn_rate
+        y_hat = prediction(X[i],W,b)
+        if y[i]!= y_hat:
+            if y_hat == 0:
+                W[0] += X[i][0]*learn_rate
+                W[1] += X[i][1]*learn_rate
+                b += learn_rate
+            elif y_hat == 1:
+                W[0] -= X[i][0]*learn_rate
+                W[1] -= X[i][1]*learn_rate
+                b -= learn_rate
+    # Fill in code
     # Fill in code
     return W, b
     
